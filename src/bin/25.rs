0000042080175a9d4ba069cda5c44436a3377335
Matej Janežič <janezic.mj@gmail.com>
2022-12-13 23:56:32 +0100

    solution: cleanup day25 part1

--- src/bin/25.rs
+++ src/bin/25.rs
@@ -1,4 +1,7 @@
 const ONE: u64 = 20151125;
+const MULTIPLY: u64 = 252533;
+const MOD: u64 = 33554393;
+
 pub fn part_one(input: &str) -> Option<u64> {
     let l = input.len();
     let (row, column): (u32, u32) = input[80..l - 1]
@@ -10,13 +13,13 @@ pub fn part_one(input: &str) -> Option<u64> {
     let mut c = 1;
     let mut code = ONE;
     while c < n {
-        code = (code * 252533) % 33554393;
+        code = (code * MULTIPLY) % MOD;
         c += 1;
     }
 
     Some(code)
 }
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_two(_input: &str) -> Option<u32> {
     None
 }
 fn main() {
