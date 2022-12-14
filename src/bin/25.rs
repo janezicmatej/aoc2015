000004308b3b81b6738f6cd0b7ca1a9638fd01ff
Matej Janežič <janezic.mj@gmail.com>
2022-12-14 10:44:17 +0100

    cleanup: finished all 25 days

--- src/bin/25.rs
+++ src/bin/25.rs
@@ -25,7 +25,6 @@ pub fn part_two(_input: &str) -> Option<u32> {
 fn main() {
     let input = &aoc::read_file("inputs", 25);
     aoc::solve!(1, part_one, input);
-    aoc::solve!(2, part_two, input);
 }
 #[cfg(test)]
 mod tests {
@@ -34,10 +33,5 @@ mod tests {
     fn test_part_one() {
         let input = aoc::read_file("test_inputs", 25);
         assert_eq!(part_one(&input.trim()), Some(7726640));
-    }
-    #[test]
-    fn test_part_two() {
-        let input = aoc::read_file("test_inputs", 25);
-        assert_eq!(part_two(&input.trim()), None);
     }
 }
